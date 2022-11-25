fd29c698b4c06eca4c8ac36856599df9d7a85ee5
Juliano-Formigari <julianomix@gmail.com>
2022-11-24 22:21:57 -0300

    config: adicionado configurações de exibição na tela admin

--- django_basico/core/admin.py
+++ django_basico/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Produto, Cliente
 # Register your models here.
+
+class ProdutoAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Cliente)
